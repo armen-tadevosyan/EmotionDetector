5020074a9285e07a84f7be5a89d80975659871be
RahulR30 <sdrahul.rao@gmail.com>
2026-02-23 17:03:12 -0500

    Finisehd editing the following files: - main.py - model.py

---
+++ model.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class EmotionCNN(nn.Module):
+    def __init__(self, num_classes=7):
+        super(EmotionCNN, self).__init__()
+        
+        # Block 1: Feature Extraction
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # Block 2: Deepening the network
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        
+        # Block 3: Fully Connected
+        self.fc1 = nn.Linear(128 * 12 * 12, 512)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = F.relu(self.bn3(self.conv3(x)))
+        
+        x = x.view(x.size(0), -1) # Flatten
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.fc2(x)
+        return x
